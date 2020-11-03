9b0c4dc6ebedab4966b8bdc34250a120d6474c1b
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2020-11-03 12:35:29 +0530

    Create raid.py

---
+++ userbot/plugins/raid.py
@@ -0,0 +1,11 @@
+#only for raids, if found misusing plugin would be killed 
+import asyncio
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern="raid", outgoing=True))
+async def movie_search(event):
+    """get movie from channel"""
+    async for msg in event.client.iter_messages(1466974713):
+     if msg:
+      await event.client.send_message(event.chat_id, msg)
+      await event.delete()
